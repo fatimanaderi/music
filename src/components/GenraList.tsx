06447db512f9718bdff2e83826fa6bdeaf6d136d
Fatima Naderi <fatimaanaderi@gmail.com>
2023-10-06 21:22:26 +0330

    handeling err

--- src/components/GenraList.tsx
+++ src/components/GenraList.tsx
@@ -1,11 +1,11 @@
-import { Center, HStack, Icon, List, ListItem, Text } from "@chakra-ui/react";
+import {  HStack, Icon, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import {SiApplemusic} from "react-icons/si"
 const GenraList = () => {
   const {data , error} = useGenres()
+  if(error) return null
   return (
     <>
-      {error && <Text>{error}</Text> }
       <List mx={1}>{data?.map((genra)=>(<ListItem key={genra.id}>
         <HStack my={4}>
           <Icon boxSize={"30px"} as={SiApplemusic} color={"purple.200"} />
--- src/components/MusicGrid.tsx
+++ src/components/MusicGrid.tsx
@@ -1,20 +1,20 @@
-import { Center, SimpleGrid, Text } from "@chakra-ui/react";
+import { Center, SimpleGrid } from "@chakra-ui/react";
 import MusicCard from "./MusicCard";
 import useMusics from "../hooks/useMusics";
 
 const MusicGrid = () => {
   const { data, error ,imageLoading } = useMusics();
+  if(error) return null
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding={10}
         spacing={8}
       >
         {data.map((music,index) => (
-          <Center>
-            <MusicCard key={music.id} music={music} imageStatus={imageLoading[index]}/>
+          <Center key={music.id}>
+            <MusicCard music={music} imageStatus={imageLoading[index]}/>
           </Center>
           
         ))}
