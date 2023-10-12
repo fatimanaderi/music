711216cc71f76a1bcc29aef8b07c614f70dd2357
Fatima Naderi <fatimaanaderi@gmail.com>
2023-10-12 16:18:24 +0330

    remove extra code

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,17 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL :"https://musicbrainz.org/ws/2",
     headers: {
-        // 'Access-Control-Allow-Origin': 'http://localhost:5173/',
-        // "Access-Control-Allow-Methods": "GET, POST, DELETE, PUT",
-        // "Access-Control-Allow-Headers":" X-PINGOTHER ,X-Requested-With, Content-Type, Authorization, Origin, Accept",
-
-        // "access-control-allow-headers" : "Content-Type, Authorization, Content-Length, X-Requested-With",
-        // "access-control-allow-methods" :"GET,PUT,POST,DELETE,OPTIONS",
         "Content-Type": "text/plain",
-        // "Access-Control-Max-Age": 86400
-
       },
-    // params:{
-    //     key:""
-    // }
 })
