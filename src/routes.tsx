87c01364999aca0926f6c2f535f55317e05b0676
Fatima Naderi <fatimaanaderi@gmail.com>
2023-11-01 10:57:34 +0330

    handle error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import MusicDetailPage from "./pages/MusicDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 
 const router = createBrowserRouter([
     {
         path :"/",
         element : <Layout/>,
+        errorElement : <ErrorPage/>,
         children : [
             {index : true , element : <HomePage/>},
             {path : "music/:id" , element : <MusicDetailPage/>}
