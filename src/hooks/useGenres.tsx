e0370706781f444fe35d4c95ee962fea402813e6
Fatima Naderi <fatimaanaderi@gmail.com>
2023-11-03 01:08:39 +0330

    change height genre and data length useGenre

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -9,7 +9,7 @@ const apiClient = new APIClient<Genre>("/genre/all");
 const useGenres = () =>
   useInfiniteQuery<Genre[] ,Error>({
     queryKey: ["genres"],
-    queryFn: ({ pageParam = 1 }) =>apiClient.getAll({ params: {limit:20,fmt: "json", offset: pageParam } },"genres"),
+    queryFn: ({ pageParam = 1 }) =>apiClient.getAll({ params: {limit:30,fmt: "json", offset: pageParam } },"genres"),
     getNextPageParam: (lastPage, allPage) => lastPage.length > 0 ? allPage.length + 1 : undefined,
     initialPageParam:[{id:1 , name:""}],
     staleTime:24*60*60*1000
