f4a77d1c76e271bd68f3208e833ab3c46271c55e
Fatima Naderi <fatimaanaderi@gmail.com>
2023-11-03 19:57:49 +0330

    change responsive music details

--- src/pages/MusicDetailPage.tsx
+++ src/pages/MusicDetailPage.tsx
@@ -10,10 +10,10 @@ const MusicDetailPage = () => {
       templateRows={{ base: "repeat(2, 1fr)", md: "repeat(1, 1fr)" }}
       templateColumns={"repeat(7, 1fr)"}
     >
-      <GridItem colSpan={{ base: 7, md: 2}} width={"100%"}>
+      <GridItem colSpan={{ base: 7, md: 3 , lg:2}} width={"100%"}>
         <MusicImgDetails id={id!} />
       </GridItem>
-      <GridItem colSpan={{ base: 7, md: 5}}>
+      <GridItem colSpan={{ base: 7, md: 3 , lg:5}}>
         <MusicDetails id={id!}/>
       </GridItem>
     </Grid>
